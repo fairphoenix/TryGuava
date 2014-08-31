17917f1b829f3b0152023724f40c33f9f721b4df
Anatoliy Shkabara <anatoliy.shkabara@gmail.com>
2014-08-31 17:40:51 +0300

    create

---
+++ src/main/java/CollectionsTest.java
@@ -0,0 +1,12 @@
+import com.google.common.collect.Maps;
+
+import java.util.Map;
+
+/**
+ * Created by anatoliy on 31.08.14.
+ */
+public class CollectionsTest {
+    public static void main(String[] args) {
+        Map<String, String> map = Maps.newConcurrentMap();
+    }
+}
