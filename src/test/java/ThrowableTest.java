df5c0d6afd9ddf7b3a636d21ff27ec205a75e8fa
Anatoliy Shkabara <anatoliy.shkabara@gmail.com>
2014-09-21 10:29:48 +0300

    added cache

---
+++ src/test/java/ThrowableTest.java
@@ -0,0 +1,45 @@
+import com.google.common.base.Throwables;
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.concurrent.*;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by anatoliy on 21.09.14.
+ */
+public class ThrowableTest {
+    @Test
+    public void testGetCausalChain() {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        List<Throwable> throwables = null;
+
+        Callable<FileInputStream> fileCallable = new
+                Callable<FileInputStream>() {
+                    @Override
+                    public FileInputStream call() throws Exception {
+                        return new FileInputStream("Bogus file");
+                    }
+                };
+
+        Future<FileInputStream> fisFuture = executor.submit(fileCallable);
+
+        try {
+            fisFuture.get();
+        } catch (Exception e) {
+//            Throwables.propagate(e);
+            throwables = Throwables.getCausalChain(e);
+        }
+
+        System.out.println(Throwables.getStackTraceAsString(throwables.get(0)));
+        System.out.println(Throwables.getStackTraceAsString(throwables.get(1)));
+
+        assertThat(throwables.get(0).getClass().isAssignableFrom(ExecutionException.class),is(true));
+        assertThat(throwables.get(1).getClass().isAssignableFrom(FileNotFoundException.class),is(true));
+        executor.shutdownNow();
+    }
+}
